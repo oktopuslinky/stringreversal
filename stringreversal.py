b48744df1cb86233dc3e584c6d8d411a2eed195a
Devansh Agrawal <devagr3924@gmail.com>
2020-08-25 11:50:23 -0500

    Finished

--- stringreversal.py
+++ stringreversal.py
@@ -1,29 +1,29 @@
 class StringReversal:
-    def __init__(self, string_input, orig_list, new_list, new_string):
+    def __init__(self, string_input, input_list, reverse_list, new_reverse_string):
         self.string_input=string_input
-        self.orig_list=orig_list
-        self.new_list=new_list
-        self.new_string=new_string
+        self.input_list=input_list
+        self.reverse_list=reverse_list
+        self.new_reverse_string=new_reverse_string
 
     def listify(self):
-        self.orig_list=list(self.string_input)
+        self.input_list=list(self.string_input)
 
-    def a_new_list(self):
-        while len(self.orig_list)!=0:
-            self.new_list.append(self.orig_list[-1])
-            self.orig_list.pop(-1)
+    def list_reverser(self):
+        while len(self.input_list)!=0:
+            self.reverse_list.append(self.input_list[-1])
+            self.input_list.pop(-1)
     
     def stringify(self):
-        for i in self.new_list:
-            self.new_string+=i
+        for i in self.reverse_list:
+            self.new_reverse_string+=i
         
     def __str__(self):
-        return f"Your original string was {self.string_input}, and your new string is {self.new_string}"
+        return f"Your original string was {self.string_input}, and your new string is {self.new_reverse_string}"
 
 
 inputter=input('What string do you want to reverse:')
 my_string=StringReversal(inputter, [], [], "")
 my_string.listify()
-my_string.a_new_list()
+my_string.list_reverser()
 my_string.stringify()
 print(my_string)
