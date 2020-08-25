ee6afbeb2846611b6932d68d17838a310a6a9737
Devansh Agrawal <devagr3924@gmail.com>
2020-08-25 11:32:32 -0500

    finished

---
+++ stringreversal.py
@@ -0,0 +1,31 @@
+class StringReversal:
+    def __init__(self, input, orig_list, new_list, new_string):
+        self.input=input
+        self.orig_list=orig_list
+        self.new_list=new_list
+        self.new_string=new_string
+
+
+    def listify(self):
+        self.orig_list=list(self.input)
+
+    
+    def a_new_list(self):
+        while len(self.orig_list)!=0:
+            self.new_list.append(self.orig_list[-1])
+            self.orig_list.pop(-1)
+    
+    def stringify(self):
+        for i in self.new_list:
+            self.new_string+=i
+        
+    def __str__(self):
+        return f"Your original string was {self.input}, and your new string is {self.new_string}"
+
+
+inputter=input('What string do you want to reverse:')
+my_string=StringReversal(inputter, [], [], "")
+my_string.listify()
+my_string.a_new_list()
+my_string.stringify()
+print(my_string)
